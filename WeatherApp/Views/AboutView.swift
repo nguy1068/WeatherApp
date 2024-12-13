a07d022a0e59176e717ed4aacb05305f0ef99e5e
nguy1068 <nguy1068@algonquinlive.com>
2024-12-13 03:33:39 -0500

    Implement UI for About View

--- WeatherApp/Views/AboutView.swift
+++ WeatherApp/Views/AboutView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct AboutView: View {
-
     var body: some View {
-        Text("About View")
-            .font(.largeTitle)
+        VStack {
+            VStack {
+                // Display the app logo
+                Image("app_logo") // Replace with your actual image name
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150)
+                // App information
+                Text("Weather App")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Text("version 1.0.0")
+                    .font(.body)
+                    .foregroundColor(.gray)
+
+                Text("This app was developed to provide users with accurate weather data and a user-friendly experience.")
+                    .font(.body)
+                    .multilineTextAlignment(.center)
+            }
             .padding()
+            .padding(.bottom, 40)
+
+            // Creator information
+            VStack {
+                Image("developer") // Replace with your actual image name
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                Text("Created by Dat Nguyen (Mike)")
+                    .font(.headline)
+            }
+            Spacer()
+        }
+        .padding()
     }
 }
 
